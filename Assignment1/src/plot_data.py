e35a1ddea2c2e762ce9fa4a3a259c99c6c0a26ab
Manuel Diaz <manu.guerra.diaz@gmail.com>
2024-11-12 12:33:38 +0000

    some plot_data updates

--- Assignment1/src/plot_data.py
+++ Assignment1/src/plot_data.py
@@ -1,6 +1,27 @@
 from matplotlib import pyplot as plt
 from utils import MAXIMUM_NUMBER_EDGES, import_data
 
+def plot_y_number_of_solutions_tested_x_number_of_vertices(results, name, log=False, save=False, show=False):
+    for max_edges in MAXIMUM_NUMBER_EDGES:
+        x = []
+        y = []
+        for size in range(4, 29):
+            x.append(size)
+            y.append(results[max_edges][size].solutions)
+        if log:
+            plt.semilogy(x, y, label=f"edges ratio: {max_edges}")
+        else:
+            plt.plot(x, y, label=f"edges ratio: {max_edges}")
+        plt.xlabel("Number of vertices")
+        plt.ylabel("Number of solutions tested")
+        plt.title(f"Number of solutions tested vs number of vertices for {name} algorithm")
+        plt.legend()
+    if save:
+        plt.savefig(f"../charts/{name}_number_of_solutions_tested_vs_number_of_vertices.png")
+    if show:
+        plt.show()
+    plt.close()
+
 
 def plot_y_number_operations_x_number_of_vertices(results, name, log=False, save=False, show=False):
     for max_edges in MAXIMUM_NUMBER_EDGES:
@@ -44,41 +65,19 @@ def plot_y_time_x_number_of_vertices(results, name, log=False, save=False, show=
         plt.show() 
 
 
-def plot_y_number_of_solutions_tested_x_number_of_vertices(results, name, log=False, save=False, show=False):
-    for max_edges in MAXIMUM_NUMBER_EDGES:
-        x = []
-        y = []
-        for size in range(4, 29):
-            x.append(size)
-            y.append(results[max_edges][size].solutions)
-        if log:
-            plt.semilogy(x, y, label=f"edges ratio: {max_edges}")
-        else:
-            plt.plot(x, y, label=f"edges ratio: {max_edges}")
-        plt.xlabel("Number of vertices")
-        plt.ylabel("Number of solutions tested")
-        plt.title(f"Number of solutions tested vs number of vertices for {name} algorithm")
-        plt.legend()
-
-    if save:
-        plt.savefig(f"../charts/{name}_number_of_solutions_tested_vs_number_of_vertices.png")
-    if show:
-        plt.show()
-    plt.close()
-
-
 def main():
     
     bruteforce_results = import_data("../results/results_complete_bruteforce.pickle")
     #greedy_results = import_data("../results/results_complete_greedy.pickle")
 
-    #plot_y_number_operations_x_number_of_vertices(bruteforce_results, "bruteforce", log=True, save=True)
-    plot_y_time_x_number_of_vertices(bruteforce_results, "bruteforce", log=True, save=True)
     #plot_y_number_of_solutions_tested_x_number_of_vertices(bruteforce_results, "bruteforce", log=True, save=True)
+    #plot_y_number_operations_x_number_of_vertices(bruteforce_results, "bruteforce", log=True, save=True)
+    #plot_y_time_x_number_of_vertices(bruteforce_results, "bruteforce", log=True, save=True)
 
+    # plot_y_number_of_solutions_tested_x_number_of_vertices(greedy_results, "greedy", log=True, save=True)
     # plot_y_number_operations_x_number_of_vertices(greedy_results, "greedy", log=True, show=True)
     # plot_y_time_x_number_of_vertices(greedy_results, "greedy", log=True, show=True)
-    # plot_y_number_of_solutions_tested_x_number_of_vertices(greedy_results, "greedy", log=True, save=True)
+    
 
     
 
